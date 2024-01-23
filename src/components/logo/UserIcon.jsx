59631d49e12bcf50db8fcee2d5896e8a21266e8a
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2024-01-23 11:17:27 +0530

    Create UserIcon.jsx

---
+++ src/components/logo/UserIcon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserIcon() {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+      alt=""
+      className="h-10 w-10"
+    />
+  );
+}
+
+export default UserIcon;
