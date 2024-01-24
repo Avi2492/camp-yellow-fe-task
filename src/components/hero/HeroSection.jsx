590d2700343fe6a4190eba1891eef98871343b8e
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2024-01-24 16:54:16 +0530

    Update HeroSection.jsx

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -43,7 +43,7 @@ function HeroSection() {
           </div>
           <p className="text-gray-400">Released: {movie?.release_date}</p>
           <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
-            {truncateString(movie?.overview, 150)}
+            {truncateString(movie?.overview, 100)}
           </p>
         </div>
       </div>
