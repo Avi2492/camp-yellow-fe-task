2d4397593f82b6321b5f365b9edc6addd093c8a1
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2024-01-24 16:57:07 +0530

    Update UserIcon.jsx

--- src/components/logo/UserIcon.jsx
+++ src/components/logo/UserIcon.jsx
@@ -5,7 +5,7 @@ function UserIcon() {
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
       alt=""
-      className="h-10 w-10"
+      className="h-10 w-10 rounded-lg"
     />
   );
 }
